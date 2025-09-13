e11798ba3bac0407e587278c49263ef3ce09d61a
Khayer Hossain <khayerhossain62@gmail.com>
2025-09-13 23:56:09 +0600

    Update page.js
    root page.js updated

--- src/app/(withCommonLayouts)/page.js
+++ src/app/(withCommonLayouts)/page.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 import AboutSection from "@/components/HomePage/AboutSection";
 import HeroSection from "@/components/HomePage/HeroSection";
 import React from "react";
-=======
 
 import WhyChooseUs from "@/components/HomePage/WhyChooseUs";
 import FAQSection from "@/UI/HomePage/FAQSection";
-import HeroSection from "@/UI/HomePage/HeroSection";
->>>>>>> c1c5b3c4c574e6fa0dfed1047e612b391b28b686
+
 
 export default async function HomeScreen() {
   return (

50cb2406144bc5e18dde6f9f4d84807699e41fbf
tomal7010 <badhonbiswast@gmail.com>
2025-09-14 22:49:10 +0600

    review & whychooseUs update

--- src/components/HomePage/ReviewsSection.jsx
+++ src/components/HomePage/ReviewsSection.jsx
@@ -57,7 +57,7 @@ export default function ReviewsSection() {
         </h2>
 
         <Swiper
-          modules={[ Pagination]}
+          modules={[Navigation, Pagination]}
           spaceBetween={30}
           slidesPerView={1}
           navigation

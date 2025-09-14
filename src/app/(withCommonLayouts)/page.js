3fde59ea2aa16238bf8ce4340a8eb92e9d6a86f5
sabithossen4 <sabithossenkhoka420@gmail.com>
2025-09-14 21:21:53 +0600

    Popular skills add

--- src/app/(withCommonLayouts)/page.js
+++ src/app/(withCommonLayouts)/page.js
@@ -5,6 +5,7 @@ import React from "react";
 import WhyChooseUs from "@/components/HomePage/WhyChooseUs";
 import FAQSection from "@/UI/HomePage/FAQSection";
 import ReviewsSection from "@/components/HomePage/ReviewsSection";
+import PopularSkills from "@/components/HomePage/PopularSkills";
 
 
 export default async function HomeScreen() {
@@ -15,8 +16,8 @@ export default async function HomeScreen() {
       <FAQSection></FAQSection>
       <WhyChooseUs></WhyChooseUs>
       <ReviewsSection></ReviewsSection>
-
-    
+      <PopularSkills></PopularSkills>
+          
     </>
   );
 }
